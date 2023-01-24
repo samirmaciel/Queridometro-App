ae36b2c5ca9d2807351a663fb0500859deb4ed1e
Samir Maciel <samirmaciel@mc1global.com>
2023-01-23 21:08:01 -0300

    create register screen

---
+++ app/src/main/java/com/samirmaciel/queridometroapp/view/register/RegisterFragment.kt
@@ -0,0 +1,30 @@
+package com.samirmaciel.queridometro.view.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.samirmaciel.queridometro.R
+import com.samirmaciel.queridometro.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private var mBinding: FragmentRegisterBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupBinding(view)
+    }
+
+    private fun setupBinding(view: View) {
+        mBinding = FragmentRegisterBinding.bind(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
+    }
+
+}
